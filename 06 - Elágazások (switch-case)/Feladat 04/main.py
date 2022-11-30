17c9be9beb5d41078d594b464de10ca70fc08686
beniiiiiiiii <b1csoben@vasvari.org>
2022-11-30 09:42:17 +0100

    match match

---
+++ 06 - Elágazások (switch-case)/Feladat 04/main.py
@@ -0,0 +1,27 @@
+num1:int=None
+num2:int=None
+math:str=None
+result:int=None
+
+print("Kérek egy számot")
+num1=int(input())
+print("Kérek egy másik számot")
+num2=int(input())
+print("Kérek egy műveletet: +, -, *, /")
+math=str(input())
+
+match math:
+    case "+":
+        result=num1+num2
+        print(f"{result}")
+    case "-":
+        result=num1-num2
+        print(result)
+    case "*":
+        result=num1*num2
+        print(result)
+    case "/":
+        result=num1/num2
+        print(result)
+    case _:
+        print("Ilyet nem tidok számolni")
