16ea2d00b70b3ba3d07024ab1b0f7b5256e67505
beniiiiiiiii <b1csoben@vasvari.org>
2022-12-01 12:57:11 +0100

    col2

---
+++ 06 - Elágazások (switch-case)/Feladat 05/main.py
@@ -0,0 +1,10 @@
+res1:int=None
+res2:int=None
+p:int=None
+s:int=None
+
+print("Kérem adja meg az első ellenállás mértékét")
+res1=int(input())
+print("Kérem adja meg a másik ellenállás mértékét")
+res2=int(input())
+
