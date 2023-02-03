0a8b3d90bd4fc92e9faf96626862e02cd4de3423
beniiiiiiiii <b1csoben@vasvari.org>
2023-02-03 07:55:26 +0100

    2023-02-03

--- 08 - While Ciklus/Feladat 05/main.py
+++ 08 - While Ciklus/Feladat 05/main.py
@@ -1,1 +1,21 @@
 maximum:int=None
+temp:str=None
+number:str=None
+osszeg:int=0
+db:int=0
+
+print("Kérem adjon meg egy maximum értéket")
+maximum=int(input())
+
+if(maximum>=100):
+    while (osszeg<maximum):
+        print("Kérek egy számot")
+        temp=input()
+        temp.isnumeric()
+        number=int(temp)
+        osszeg+=number
+        db+=1
+        print(f"A számok összege: {osszeg}")
+        print(f"Próbálkozások száma: {db}")
+if osszeg>=maximum:
+    print(f"{db} lépésben érte el a megadott határértéket")
---
+++ 08 - While Ciklus/Feladat 06/main.py
@@ -0,0 +1,16 @@
+age:int=None
+temp:str=None
+
+while(age==None):
+    print("Kérem adjon meg egy életkort")
+    temp=int(input())
+    if (temp.isnumeric()):
+        number=int(temp)
+    if (age>= 0 and age<=6):
+        print("Gyerekkor")
+    if (age>=7 and age<=18):
+        print("iskoláskor")
+    if (age>=19 and age <=65):
+        print("Dolgozókor")
+    if (age>65):
+        ("Nyugdíjas")
